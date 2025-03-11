fe47aa7450e8552d112e8edc2edd8c78f31c9322
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-03-11 23:30:50 +0600

    soketInstall

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default function App() {
+  
   return (
     <div >Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, qui.</div>
   )
---
+++ server/index.js
@@ -0,0 +1,6 @@
+const express = require("express")
+const http = require("http")
+const app = express()
+app.listen(3000, () => {
+    console.log("Server Is Running")
+})
