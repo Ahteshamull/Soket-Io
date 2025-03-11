20ba41c4631afaef401421f452b6cd10fd438f9a
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-03-12 00:13:27 +0600

    38:41

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
-import {io} from "socket.io-client"
+import React, { useEffect } from 'react'
+import { io } from "socket.io-client"
+const socket = io("http://localhost:3000")
+
 export default function App() {
-  
+  useEffect(() => {
+   socket.emit("")
+ },[])
   return (
     <div >Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, qui.</div>
   )
